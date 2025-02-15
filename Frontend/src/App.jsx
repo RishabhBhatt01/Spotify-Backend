ad6fc6b7a293f657cb44fa9cfea1a6b44c48a8eb
rishabh bhatt <rishabhbhatt088@gmail.com>
2025-02-15 18:19:52 +0530

    Cookies added to app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,44 @@
 import { useState } from 'react'
 
 import '../src/App.css'
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {BrowserRouter,Routes,Route, Navigate} from "react-router-dom";
 import LoginComponent from './routes/Login';
 import SignupComponent from './routes/Signup';
 import HomeComponent from "./routes/Home";
+import { useCookies } from 'react-cookie';
 
 
 
 
 function App(){
+  const [cookie,setCookie] = useCookies(["token"]);
+  cookie.token;
+
+
+
+
+
   return <div className='w-screen h-screen custom-font'>
 
 
     <BrowserRouter>
+      {cookie.token ? ( // this means if cookie.token exists
+
+
+      <Routes>
+        <Route path="/home" element = {<HomeComponent/>}  />
+        <Route path='*' element={<Navigate to="/home" />}/>
+      </Routes>
+
+) : (
       <Routes>
         <Route path="/login" element = {<LoginComponent/>} />
         <Route path='/signup' element= {<SignupComponent/>} />
         <Route path="/home" element = {<HomeComponent/>}  />
-      </Routes>
+        <Route path='*' element={<Navigate to="/login" />} />
+  
+  </Routes>
+ )}
     </BrowserRouter>
     
   </div>
